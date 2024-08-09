f54a634081990a4fe05610555ff2e0040d95df16
saurabh kamble <saurabh2836@gmail.com>
2024-08-09 06:51:16 +0530

    adding new tailwind project

---
+++ tailwindcss/src/sections/index.js
@@ -0,0 +1,18 @@
+
+
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import Services from "./Services";
+import SpecialOffer  from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import PopularProducts from "./PopularProducts";
+import Footer from "./Footer";
+
+
+export {
+    Hero,CustomerReviews,Services,SpecialOffer,
+    Subscribe,SuperQuality,PopularProducts,Footer
+}
+
+
