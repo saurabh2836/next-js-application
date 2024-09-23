5e326ed524826917cd3688f367bd0ca10234ff03
saurabh kamble <saurabh2836@gmail.com>
2024-09-23 22:06:27 +0530

    adding page insight from vercel

--- js-mastery-nextjs-course/app/(root)/layout.tsx
+++ js-mastery-nextjs-course/app/(root)/layout.tsx
@@ -2,6 +2,7 @@ import LeftSidebar from "@/components/shared/LeftSidebar";
 import Navbar from "@/components/shared/navbar/Navbar";
 import RightSidebar from "@/components/shared/RightSidebar";
 import React from "react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -11,6 +12,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         <LeftSidebar />
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
+          <SpeedInsights />
         </section>
         <RightSidebar />
       </div>
